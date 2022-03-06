27557aa6bb144f63396bf6d015c57cfd9ab6eaf7
André Díaz <andre290800@gmail.com>
2022-03-06 17:24:08 -0600

    feat: Home Page

--- src/components/homePage.jsx
+++ src/components/homePage.jsx
@@ -1,9 +1,15 @@
 import React from "react";
+import { LinkContainer } from "react-router-bootstrap";
+import Button from "react-bootstrap/Button";
 
 function HomePage(props) {
   return (
     <div>
-      <h1>Home Page</h1>
+      <h1>¡Bienvenido a Sugar Planet!</h1>
+      <p>Puedes realizar tu pedido a continuación:</p>
+      <LinkContainer to={"/order"}>
+        <Button variant="primary">Ordena ahora</Button>
+      </LinkContainer>
     </div>
   );
 }
