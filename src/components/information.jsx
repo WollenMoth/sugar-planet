d7393245ae8df677e6b159f18d6c31d0bd3104bc
André Díaz <andre290800@gmail.com>
2022-03-06 17:51:12 -0600

    feat: Information

--- src/components/information.jsx
+++ src/components/information.jsx
@@ -3,7 +3,13 @@ import React from "react";
 function Information(props) {
   return (
     <div>
-      <h1>Information</h1>
+      <h2>Dirección</h2>
+      <p>Av. Paseo de la Reforma 256, Ciudad de México, CDMX</p>
+      <h2>Teléfono</h2>
+      <p>555 555 1212</p>
+      <h2>Horarios de atención</h2>
+      <p>Lunes a Viernes de 9:00 a 18:00</p>
+      <p>Sábado de 9:00 a 12:00</p>
     </div>
   );
 }
