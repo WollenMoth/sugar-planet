5d634f6f40611c4ef928f10de3713f1a3c2918c6
André Díaz <andre290800@gmail.com>
2022-03-06 16:50:39 -0600

    feat: add routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { Fragment } from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+import Container from "react-bootstrap/Container";
+import "./App.css";
+import NavBar from "./components/navBar";
+import HomePage from "./components/homePage";
+import Dashboard from "./components/dashboard";
+import Information from "./components/information";
+import CakeForm from "./components/cakeForm";
+import NotFound from "./components/notFound";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <NavBar />
+      <Container>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/info" element={<Information />} />
+          <Route path="/order" element={<CakeForm />} />
+          <Route path="/not-found" element={<NotFound />} />
+          <Route path="*" element={<Navigate to="/not-found" />} />
+        </Routes>
+      </Container>
+    </Fragment>
   );
 }
 
