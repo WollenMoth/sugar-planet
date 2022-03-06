811e81d1f4039a8d0310d589e9b4fbdb83469455
André Díaz <andre290800@gmail.com>
2022-03-06 17:45:27 -0600

    feat: Cake Form

--- src/components/cakeForm.jsx
+++ src/components/cakeForm.jsx
@@ -1,10 +1,48 @@
 import React from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+
+const flavours = ["Chocolate", "Fresa", "Vainilla", "Limón"];
+const decorations = ["Figuras de Fondant", "Chispas de Chocolate", "Betún"];
 
 function CakeForm(props) {
   return (
-    <div>
-      <h1>Cake Form</h1>
-    </div>
+    <Form>
+      <Form.Group className="mb-3" controlId="description">
+        <Form.Label>Descripción general</Form.Label>
+        <Form.Control as="textarea" rows={3} />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="flavours">
+        <Form.Label>Sabor</Form.Label>
+        {flavours.map((flavour) => {
+          return <Form.Check type="checkbox" label={flavour} />;
+        })}
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="decorations">
+        <Form.Label>Decoración</Form.Label>
+        {decorations.map((decoration) => {
+          return <Form.Check type="checkbox" label={decoration} />;
+        })}
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="name">
+        <Form.Label>Nombre</Form.Label>
+        <Form.Control type="text" placeholder="John Smith" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="phone">
+        <Form.Label>Teléfono</Form.Label>
+        <Form.Control type="tel" placeholder="555 555 1212" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="email">
+        <Form.Label>Correo electrónico</Form.Label>
+        <Form.Control type="email" placeholder="name@domain.com" />
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        Enviar
+      </Button>{" "}
+      <Button variant="secondary" type="reset">
+        Reset
+      </Button>
+    </Form>
   );
 }
 
