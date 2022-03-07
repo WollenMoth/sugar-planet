3e4b355fb65ab56a2a4a00c22776cb8e5828c2ed
André Díaz <andre290800@gmail.com>
2022-03-06 20:27:51 -0600

    style: add logo to NavBar

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -3,13 +3,21 @@ import { LinkContainer } from "react-router-bootstrap";
 import Navbar from "react-bootstrap/Navbar";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
+import logo from "../assets/logo.png";
 
 function NavBar(props) {
   return (
     <Navbar bg="dark" variant="dark" className="mb-2">
       <Container>
         <LinkContainer to="/">
-          <Navbar.Brand>Sugar Planet</Navbar.Brand>
+          <Navbar.Brand>
+            <img
+              src={logo}
+              height="30"
+              className="d-inline-block align-top"
+              alt="Sugar Planet logo"
+            />
+          </Navbar.Brand>
         </LinkContainer>
         <Nav className="me-auto">
           <LinkContainer to="/">
